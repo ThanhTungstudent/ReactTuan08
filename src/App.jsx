ad81019b970a7e5492cf347d16c00009d2613633
ThanhTung1909 <phamthanhtung200698@gmail.com>
2025-04-17 11:16:45 +0700

    8&9 isActive + Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout/MainLayout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import { useEffect, useState } from "react";
+import Teams from "./pages/Teams/Teams";
+import Analytics from "./pages/Analytics/Analytics";
+import Messages from "./pages/Messages/Messages";
+import Integration from "./pages/Integration/Integration";
+import Projects from "./pages/Projects/Projects";
 
 function App() {
   const [overviews, setOverviews] = useState([]);
@@ -37,12 +42,16 @@ function App() {
     return <div>Error: {error}</div>;
   }
 
-  
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout overviews={overviews} />}>
           <Route index element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/integration" element={<Integration />} />
         </Route>
       </Routes>
     </BrowserRouter>
